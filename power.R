047538a7e8e99fe9454cc24c6a0f30349f5ee5c9
Daniel-Gundersen <DanielA_Gundersen@DFCI.HARVARD.EDU>
2017-06-26 10:54:16 -0400

    added progress bar

--- power.R
+++ power.R
@@ -2,14 +2,22 @@ rm(list=ls())
 library(Hmisc); library(MASS); library(tidyverse); library(lme4)
 
 #power function
-f_power <- function(a_pr, b_OR, sd_a=1, sd_b=1, covar_uaub=0, t_n, samp_n, reps_n, seed)  {
+f_power <- function(a_pr, b_OR, sd_a=1, sd_b=1, covar_uaub=0, t_n, samp_n, 
+                    reps_n, seed)  {
+      progbar <- winProgressBar(title = "progress bar", min = 0,
+                           max = reps_n, width = 300)
       set.seed=seed
       b_val <- se_val <- z_val <- p_val <- rep(NULL, reps_n)
-
+      
       for (i in 1:reps_n) {
+            #progress bar to monitor simulation
+            Sys.sleep(0.1)
+            setWinProgressBar(progbar, i, title=paste(round(i/reps_n*100, 0),
+                                                  "% done"))
             #random effects for persons
-            rand_effs <- mvrnorm(samp_n, mu=c(0,0), Sigma=matrix(c(sd_a,covar_uaub,
-                                                             covar_uaub,sd_b),2,2))
+            rand_effs <- mvrnorm(samp_n, mu=c(0,0), 
+                                 Sigma=matrix(c(sd_a,covar_uaub, 
+                                                covar_uaub,sd_b),2,2))
             colnames(rand_effs) <- c("ea", "eb")
             
             #generate time and outcome variables; assemble as data frame
@@ -19,8 +27,8 @@ f_power <- function(a_pr, b_OR, sd_a=1, sd_b=1, covar_uaub=0, t_n, samp_n, reps_
                              ub_i = rep(rand_effs[,2], each=t_n))
             
             df.rep <- df.rep %>% mutate(pr_tobacco = (1 + exp(-(log(a_pr/(1-a_pr)) +
-                                                                            (ub_i+log(b_OR))*t + 
-                                                                            ua_i)))^-1,
+                                                                      (ub_i+log(b_OR))*t + 
+                                                                      ua_i)))^-1,
                                         tobacco = rbinom(samp_n*t_n, 1, pr_tobacco)) 
             #fit model
             out <- glmer(tobacco ~ 1 + t + (1 + t | person_id), 
@@ -30,5 +38,6 @@ f_power <- function(a_pr, b_OR, sd_a=1, sd_b=1, covar_uaub=0, t_n, samp_n, reps_
             z_val[i] <- summary(out)$coefficient[2,3]
             p_val[i] <- summary(out)$coefficient[2,4]
       }
+      close(progbar)
       return(tibble(b_val, se_val, z_val, p_val))
 }
