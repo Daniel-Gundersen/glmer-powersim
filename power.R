3127bf8a891dddf23a99bb36d6e73cdebc4e7eb9
Daniel-Gundersen <DanielA_Gundersen@DFCI.HARVARD.EDU>
2017-06-27 09:47:44 -0400

    set max iterations to 100000

--- power.R
+++ power.R
@@ -31,8 +31,11 @@ f_power <- function(a_pr, b_OR, sd_a=1, sd_b=1, covar_uaub=0, t_n, samp_n,
                                                                       ua_i)))^-1,
                                         tobacco = rbinom(samp_n*t_n, 1, pr_tobacco)) 
             #fit model
-            out <- glmer(tobacco ~ 1 + t + (1 + t | person_id), 
-                         data=df.rep, family=binomial(link="logit"))
+            out <- glmer(tobacco ~ 1 + t + (1 | person_id) + (-1 + t | person_id), 
+                         data=df.rep, family=binomial(link="logit"),
+                         control=glmerControl(optCtrl=list(maxfun=100000)))
+            
+            #collect parameter estimates for each i
             b_val[i] <- summary(out)$coefficient[2,1]
             se_val[i] <- summary(out)$coefficient[2,2]
             z_val[i] <- summary(out)$coefficient[2,3]
@@ -41,3 +44,6 @@ f_power <- function(a_pr, b_OR, sd_a=1, sd_b=1, covar_uaub=0, t_n, samp_n,
       close(progbar)
       return(tibble(b_val, se_val, z_val, p_val))
 }
+
+a<-f_power(a_pr=0.1, b_OR=1.3, sd_a=(pi^2/3)^0.5, sd_b=1, covar_uaub=0, 
+                    t_n=6, samp_n=2600/2*.8^2, reps_n=100, seed=1978) 
